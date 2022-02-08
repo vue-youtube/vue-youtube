644a232b1c858632f350c29d03b38a107ad614c9
Techassi <git@techassi.dev>
2022-02-08 18:45:20 +0100

    Add playground

---
+++ playground/src/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    ref="player"
+  />
+</template>
+
+<script setup lang="ts">
+import { usePlayer } from '@vue-youtube/core';
+import { ref } from 'vue';
+
+const videoId = ref('dQw4w9WgXcQ');
+const player = ref();
+
+setTimeout(() => {
+  videoId.value = 'Im_t3NX9yes';
+}, 10 * 1000);
+
+usePlayer(videoId, player, { cookie: false, playerVars: {} });
+</script>
---
+++ playground/vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@vue-youtube/component': resolve('../packages/component/index.ts'),
+      '@vue-youtube/core': resolve('../packages/core/index.ts'),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['vue-demi'],
+  },
+});
