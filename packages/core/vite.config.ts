edfc47e2441e42d4ed55b418170d1eda582cd466
Techassi <git@techassi.dev>
2022-07-14 13:55:06 +0200

    Adjust resolve alias
    Only replace the @vue-youtube/shared import when using the dev mode

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,40 +2,42 @@ import { basename, dirname, join } from 'node:path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: 'index.ts',
-      formats: ['cjs', 'es'],
-      fileName: 'index',
-    },
-    rollupOptions: {
-      external: ['vue', '@vue-youtube/core', '@vue-youtube/shared'],
-      output: {
-        globals: {
-          vue: 'Vue',
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      lib: {
+        entry: 'index.ts',
+        formats: ['cjs', 'es'],
+        fileName: 'index',
+      },
+      rollupOptions: {
+        external: ['vue', '@vue-youtube/core', '@vue-youtube/shared'],
+        output: {
+          globals: {
+            vue: 'Vue',
+          },
         },
       },
     },
-  },
-  plugins: [
-    dts({
-      tsConfigFilePath: '../../tsconfig.json',
-      exclude: 'node_modules',
-      staticImport: true,
-      outputDir: 'dist',
-      include: '.',
-      beforeWriteFile: (path: string, content: string) => {
-        return {
-          filePath: join(dirname(path).replace('src', ''), basename(path)),
-          content,
-        };
+    plugins: [
+      dts({
+        tsConfigFilePath: '../../tsconfig.json',
+        exclude: 'node_modules',
+        staticImport: true,
+        outputDir: 'dist',
+        include: '.',
+        beforeWriteFile: (path: string, content: string) => {
+          return {
+            filePath: join(dirname(path).replace('src', ''), basename(path)),
+            content,
+          };
+        },
+      }),
+    ],
+    resolve: {
+      alias: mode === 'production' ? [] : {
+        '@vue-youtube/shared': '../shared/index.ts',
       },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@vue-youtube/shared': '../shared/index.ts',
     },
-  },
+  };
 });
