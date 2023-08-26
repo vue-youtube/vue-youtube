9fc1b5d344a3082020743456c19ed7fa9fbb73a0
Techassi <git@techassi.dev>
2023-08-26 16:58:02 +0200

    Simplify vite dts plugin configuration

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,4 +1,3 @@
-import { basename, dirname, join } from 'node:path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
@@ -21,17 +20,12 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       dts({
+        exclude: ['node_modules', '*.config.ts'],
         tsconfigPath: '../../tsconfig.json',
-        exclude: 'node_modules',
         staticImport: true,
+        rollupTypes: true,
         outDir: 'dist',
         include: '.',
-        beforeWriteFile: (path: string, content: string) => {
-          return {
-            filePath: join(dirname(path).replace('src', ''), basename(path)),
-            content,
-          };
-        },
       }),
     ],
     resolve: {
